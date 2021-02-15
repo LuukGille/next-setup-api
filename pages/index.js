a0ff8dd66aeff94d93544436f1b0761adab2b2ef
Luuk Gille <lgille@competa.com>
2021-02-15 16:53:17 +0100

    removed api --example and create a plain create next app with an api hook

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,31 @@
-import useSWR from 'swr'
-import Person from '../components/Person'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css';
+import Users from '../components/UserFetch'
 
-const fetcher = (url) => fetch(url).then((res) => res.json())
-
-export default function Index() {
-  const { data, error } = useSWR('/api/people', fetcher)
-
-  if (error) return <div>Failed to load</div>
-  if (!data) return <div>Loading...</div>
-
+export default function Home() {
   return (
-    <ul>
-      {data.map((p, i) => (
-        <Person key={i} person={p} />
-      ))}
-    </ul>
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Random User API
+        </h1>
+        <Users />
+      </main>
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
   )
 }
